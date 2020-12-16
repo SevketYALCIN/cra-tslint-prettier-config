59ed83dd34c2f5480967f80f99f41f5c34c4c1cb
SevketYALCIN <sevket.yalcin@outlook.com>
2020-12-16 16:19:31 +0100

    add prettier for lint script

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const text = 'Learn React'
+  const text: string = 'Learn React'
   return (
     <div className="App">
       <header className="App-header">
